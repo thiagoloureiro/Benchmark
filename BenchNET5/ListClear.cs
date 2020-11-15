7e06e17b222087818345626897ce1fb1cdd25d87
Thiago Loureiro <thiagoguaru@me.com>
2020-11-15 20:26:59 +0100

    Included .net 5

---
+++ BenchNET5/ListClear.cs
@@ -0,0 +1,72 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BenchNET
+{
+    public class ListClear
+    {
+        private List<int> lst;
+        private List<int> lst2;
+
+        public ListClear()
+        {
+            lst = new List<int>
+            {
+                100,
+                200,
+                300,
+                400,
+                500
+            };
+        }
+
+        [Benchmark]
+        public void LstAny()
+        {
+            if (lst.Any())
+            {
+                lst.Clear();
+            }
+        }
+
+        [Benchmark]
+        public void LstAnyEmpty()
+        {
+            if (lst2.Any())
+            {
+                lst2.Clear();
+            }
+        }
+
+        [Benchmark]
+        public void lstCount()
+        {
+            if (lst.Count > 0)
+            {
+                lst.Clear();
+            }
+        }
+
+        [Benchmark]
+        public void lstCountEmpty()
+        {
+            if (lst2.Count > 0)
+            {
+                lst2.Clear();
+            }
+        }
+
+        [Benchmark]
+        public void Lst()
+        {
+            lst.Clear();
+        }
+
+        [Benchmark]
+        public void LstEmpty()
+        {
+            lst.Clear();
+        }
+    }
+}
---
+++ BenchNET5/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace BenchNET
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<Iterate>();
+			BenchmarkRunner.Run<ListClear>();
+			BenchmarkRunner.Run<Md5VsSha256>();
+
+			Console.ReadLine();
+		}
+	}
+}
